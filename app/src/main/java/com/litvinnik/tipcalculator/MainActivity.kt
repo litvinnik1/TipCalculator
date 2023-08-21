b56b6052d02aa54faadcd3813a6ac12a399446bf
Nikita Lytvyn <litvinnik1@gmail.com>
2023-08-22 02:03:44 +0300

    Added functionality to the application

--- app/src/main/java/com/litvinnik/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/litvinnik/tipcalculator/MainActivity.kt
@@ -3,10 +3,15 @@ package com.litvinnik.tipcalculator
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 10
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,9 +30,14 @@ class MainActivity : AppCompatActivity() {
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
 
-        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
 
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $p1")
+                tvTipPercentLabel.text = "$p1%"
+                computeTipAndTotal()
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -36,5 +46,33 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        etBaseAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+                computeTipAndTotal()
+            }
+
+        })
+
+    }
+
+    private fun computeTipAndTotal() {
+        if (etBaseAmount.text.isEmpty()) {
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        val baseAmount = etBaseAmount.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount = baseAmount + tipAmount
+
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text = "%.2f".format(totalAmount)
     }
 }
